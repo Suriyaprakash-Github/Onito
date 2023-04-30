4ec9b2dc675cbcb168918d4c40b4afd91b17d11a
Suriyaprakash-Github <yoursgump@gmail.com>
2023-04-30 14:01:46 +0530

    starter

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <div className="form-container">
+        <form action="" className="form"></form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
